a546376baf579f41ee86edfb83621ef811ea8a56
marbotte <bottinmarius@gmail.com>
2023-05-09 08:44:12 -0500

    added the possibility of defining synonyms between fields in the ris objects

--- refManagement/extractFields.R
+++ refManagement/extractFields.R
@@ -1,7 +1,27 @@
 
+# There are various ways to use the RIS norm in the platforms which export them. For example, why the norm states that Abstract of articles should be put in the "N2" field, Scopus and web of science put it in the "AB" field
+# Therefore, in order to keep the data from the original file as untouched as possible, we put a table "synoFields" in the object and we apply the synonymy in the object only at the moment of extracting fields
+
+synoFields <- function(extractedRis,fieldFrom=c("T1","N2"),fieldTo=c("TI","AB"))
+{
+  tab<-data.frame(from=fieldFrom,to=fieldTo)
+  if("synoFields" %in% names(extractedRis))
+  {extractedRis$synoFields<-unique(rbind.data.frame(extractedRis$synoFields,tab))}else{
+    extractedRis$synoFields <- tab 
+  }
+  return(extractedRis)
+}
 
 extractFields <- function(extractedRis, fields=c("DO","TI","AU"),sep="|")
 {
+  # Apply synonyms
+  if("synoFields" %in% names(extractedRis))
+  {
+    for(i in 1:nrow(extractedRis$synoFields))
+    {
+      extractedRis$fieldName <- sub(paste0("^", extractedRis$synoFields$from[i]), extractedRis$synoFields$to[i], extractedRis$fieldName)
+    }
+  }
   # Are fields unique by record
   tab <- data.frame(
     fieldName=extractedRis$fieldName[!is.na(extractedRis$fieldName)&extractedRis$fieldName%in%fields],
